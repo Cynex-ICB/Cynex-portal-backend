328af4771252d189c3f7f9419e8c722480e4c23d
cynexicb <cynex.ctf@gmail.com>
2026-05-29 00:00:36 +0530

    updated cors

--- server.js
+++ server.js
@@ -47,7 +47,6 @@ const corsOptions = {
 
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "https://cynexicb.com");
-  res.setHeader("Access-Control-Allow-Origin", "https://www.cynexicb.com");
   res.setHeader("Access-Control-Allow-Credentials", "true");
   res.setHeader("Access-Control-Allow-Methods", "GET,POST,PUT,PATCH,DELETE,OPTIONS");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type,Authorization");
