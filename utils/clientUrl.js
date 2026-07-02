2b8a1c9aaadb2217db991c47d6b4094adaa85e06
cynex <cynex.ctf@gmail.com>
2026-07-02 09:28:51 +0530

    updated cors in clienturl.js

--- utils/clientUrl.js
+++ utils/clientUrl.js
@@ -1,8 +1,8 @@
 export function getClientUrl() {
-  const raw = process.env.CLIENT_URL || "http://cynexicb.com";
+  const raw = process.env.CLIENT_URL || "http://app.cynexicb.com";
   return raw
     .split(",")[0]
     .trim()
-    .replace(/^https?:\/\/www\.cynexicb\.com/i, "http://cynexicb.com")
+    .replace(/^https?:\/\/www\.cynexicb\.com/i, "http://app.cynexicb.com")
     .replace(/\/$/, "");
 }
